cfc24d1e760b2749c5d053726c5967c3eb526e8d
anasmorwan <anasmorwan5@gmail.com>
2026-03-13 21:17:52 +0200

    Update quiz_buttons.py

--- bot/keyboards/quiz_buttons.py
+++ bot/keyboards/quiz_buttons.py
@@ -5,8 +5,8 @@ from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 def quiz_keyboard(bot_username):
     keyboard = InlineKeyboardMarkup(row_width=1)
     buttons = [
-        InlineKeyboardButton("📝 إبدأ", callback_data="go_generate"),
-        InlineKeyboardButton("​🚀 نشر في القناة", callback_data="quick_quiz"),
+        InlineKeyboardButton("📝 إبدأ", callback_data="start_quiz"),
+        InlineKeyboardButton("​🚀 نشر في القناة", callback_data="post_quiz"),
         ]
     keyboard.add(*buttons)
     
