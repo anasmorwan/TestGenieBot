5ff3573395da43bdce5bacb51ad1632f0f0fca78
anasmorwan <anasmorwan5@gmail.com>
2026-03-13 15:30:00 +0200

    Update quiz.py

--- models/quiz.py
+++ models/quiz.py
@@ -1,13 +1,15 @@
-# quiz.py
 class QuizQuestion:
-    def __init__(self, question, options, correct_index):
+
+    def __init__(self, question, options, correct_index, explanation=""):
         self.question = question
         self.options = options
         self.correct_index = correct_index
+        self.explanation = explanation
 
     def to_dict(self):
         return {
             "question": self.question,
             "options": self.options,
-            "correct_index": self.correct_index
+            "correct_index": self.correct_index,
+            "explanation": self.explanation
         }
