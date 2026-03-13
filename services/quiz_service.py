04d8a976ea4ea035dfdbe049077992425e0b4590
anasmorwan <anasmorwan5@gmail.com>
2026-03-13 16:06:32 +0200

    Update quiz_service.py

--- services/quiz_service.py
+++ services/quiz_service.py
@@ -1,7 +1,7 @@
 # quiz_service.py
 from ai.llm_client import generate_gemini_response
 from ai.prompts import build_quiz_prompt
-from util.json_utility import extract_json_from_string
+from utils.json_utils import extract_json_from_string
 
 
 def generate_quizzes_from_text(content, user_id, num_quizzes=5):
