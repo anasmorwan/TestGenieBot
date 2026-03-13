52a22b48444b1dc4b48434204ba9310fe0d65187
anasmorwan <anasmorwan5@gmail.com>
2026-03-13 19:13:26 +0200

    Update file_upload.py

--- bot/handlers/file_upload.py
+++ bot/handlers/file_upload.py
@@ -6,10 +6,8 @@ def handle_file_upload(msg):
 
     uid = msg.from_user.id
     chat_id = msg.chat.id
-
     file_id = msg.document.file_id
     file_name = msg.document.file_name
-
     file_info = bot.get_file(file_id)
 
     if is_file_size_allowed(bot, file_id):
@@ -19,3 +17,4 @@ def handle_file_upload(msg):
     file_data = bot.download_file(file_info.file_path)
 
     path = save_file(uid, file_name, file_data)
+    return path
