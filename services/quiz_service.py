79c9caa58c5fa99261fe7274c77335c026063736
anasmorwan <anasmorwan5@gmail.com>
2026-03-13 17:08:32 +0200

    Update quiz_service.py

--- services/quiz_service.py
+++ services/quiz_service.py
@@ -1,5 +1,5 @@
 # quiz_service.py
-from ai.llm_client import generate_gemini_response
+from ai.llm_client import generate_smart_response
 from ai.prompts import build_quiz_prompt
 from utils.json_utils import extract_json_from_string
 
@@ -8,7 +8,7 @@ def generate_quizzes_from_text(content, user_id, num_quizzes=5):
 
     prompt = build_quiz_prompt(content, num_quizzes)
 
-    response = generate_gemini_response(prompt)
+    response = generate_smart_response(prompt)
 
     quizzes = extract_json_from_string(response)
 
