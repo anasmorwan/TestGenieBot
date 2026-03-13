003a0f50184ea9d895e2e8cc62b3b4ced4405de7
anasmorwan <anasmorwan5@gmail.com>
2026-03-13 15:32:11 +0200

    Update quiz_session_service.py

--- services/quiz_session_service.py
+++ services/quiz_session_service.py
@@ -1,1 +1,21 @@
+class QuizSessionManager:
 
+    def __init__(self):
+        self.active_sessions = {}
+
+    def start_session(self, chat_id, quizzes):
+
+        self.active_sessions[chat_id] = {
+            "quizzes": quizzes,
+            "index": 0,
+            "score": 0
+        }
+
+    def get_current_question(self, chat_id):
+
+        state = self.active_sessions.get(chat_id)
+
+        if not state:
+            return None
+
+        return state["quizzes"][state["index"]]
