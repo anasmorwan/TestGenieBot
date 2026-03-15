80a1b634b1d28fc66157f772f2855cbcf2c429d2
anasmorwan <anasmorwan5@gmail.com>
2026-03-15 14:46:40 +0200

    Update pre_checkout_query_handler.py

--- bot/handlers/pre_checkout_query_handler.py
+++ bot/handlers/pre_checkout_query_handler.py
@@ -6,9 +6,3 @@ def register_payment(bot):
     def checkout(pre_checkout_query):
         bot.answer_pre_checkout_query(pre_checkout_query.id, ok=True)
 
-    # الخطوة 2: ماذا يحدث بعد نجاح الدفع؟
-    @bot.message_handler(content_types=['successful_payment'])
-    def got_payment(message):
-        # هنا يمكنك تحديث قاعدة البيانات للمستخدم
-        # message.successful_payment.invoice_payload سيعطيك "user_premium_subscription"
-        bot.send_message(message.chat.id, "شكراً لك! تم تفعيل الاشتراك بنجاح 🌟")
