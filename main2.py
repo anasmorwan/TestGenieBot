c101bdf9200119ef2d5cecd046c99fe259008423
anasmorwan <anasmorwan5@gmail.com>
2026-03-15 14:57:32 +0200

    Update main2.py

--- main2.py
+++ main2.py
@@ -2,7 +2,7 @@
 from bot.bot_instance import bot
 from bot.bot_instance import set_webhook
 
-
+import bot.handlers.pre_checkout_query_handler
 import bot.handlers.start  # يستورد /start handler
 import bot.handlers.text_handler
 import bot.handlers.file_handler
@@ -14,7 +14,7 @@ import bot.handlers.callback_handler
 text_handler.register(bot)
 file_handler.register(bot)
 callback_handler.register(bot)
-
+pre_checkout_query_handler.register_payment(bot)
 set_webhook()
 
 
