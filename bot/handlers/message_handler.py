3b9bdfc9a9cc8ab2dca5bd01ca6c8ed139e6f19f
anasmorwan <anasmorwan5@gmail.com>
2026-03-15 14:48:31 +0200

    Delete bot/handlers/message_handler.py

--- bot/handlers/message_handler.py
+++
@@ -1,6 +0,0 @@
-    # الخطوة 2: ماذا يحدث بعد نجاح الدفع؟
-    @bot.message_handler(content_types=['successful_payment'])
-    def got_payment(message):
-        # هنا يمكنك تحديث قاعدة البيانات للمستخدم
-        # message.successful_payment.invoice_payload سيعطيك "user_premium_subscription"
-        bot.send_message(message.chat.id, "شكراً لك! تم تفعيل الاشتراك بنجاح 🌟")
