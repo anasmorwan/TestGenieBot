befd2676c7093c25603c9ca023d194c573c39f49
anasmorwan <anasmorwan5@gmail.com>
2026-03-15 14:27:14 +0200

    Update quiz_service.py

--- services/quiz_service.py
+++ services/quiz_service.py
@@ -4,9 +4,9 @@ from ai.prompts import build_quiz_prompt
 from utils.json_utils import extract_json_from_string
 
 
-def generate_quizzes_from_text(content, user_id, num_quizzes=5):
+def generate_quizzes_from_text(content, user_id, user_instruction=None, num_quizzes=5):
 
-    prompt = build_quiz_prompt(content, num_quizzes)
+    prompt = build_quiz_prompt(content, num_quizzes, user_instruction=None)
 
     response = generate_smart_response(prompt)
 
