b3f5623964befc4467adb3829deb4be2357b750d
anasmorwan <anasmorwan5@gmail.com>
2026-03-15 14:30:00 +0200

    Update main2.py

--- main2.py
+++ main2.py
@@ -7,12 +7,13 @@ import bot.handlers.start  # يستورد /start handler
 import bot.handlers.text_handler
 import bot.handlers.file_handler
 from storage.sqlite_db import init_db
-
+import bot.handlers.callback_handler
 
 
 
 text_handler.register(bot)
 file_handler.register(bot)
+callback_handler.register (bot)
 
 set_webhook()
 
