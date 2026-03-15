12ed027823ad881298eda7b1c480a6deabc6a8e4
anasmorwan <anasmorwan5@gmail.com>
2026-03-15 15:14:11 +0200

    Update main2.py

--- main2.py
+++ main2.py
@@ -8,13 +8,14 @@ import bot.handlers.text_handler
 import bot.handlers.file_handler
 from storage.sqlite_db import init_db
 import bot.handlers.callback_handler
-
+import bot.handlers.payment_handler
 
 
 text_handler.register(bot)
 file_handler.register(bot)
 callback_handler.register(bot)
 pre_checkout_query_handler.register_payment(bot)
+payment_handler.register(bot)
 set_webhook()
 
 
