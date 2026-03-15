8d3b4e37c9e5d1440cb413c19a56bf8875a80c93
anasmorwan <anasmorwan5@gmail.com>
2026-03-15 14:48:18 +0200

    Update text_handler.py

--- bot/handlers/text_handler.py
+++ bot/handlers/text_handler.py
@@ -26,3 +26,12 @@ def register(bot):
 
         # بدء الاختبار مباشرة
         quiz_manager.start_quiz(chat_id, quiz_code, bot)
+
+    
+
+    # الخطوة 2: ماذا يحدث بعد نجاح الدفع؟
+    @bot.message_handler(content_types=['successful_payment'])
+    def got_payment(message):
+        # هنا يمكنك تحديث قاعدة البيانات للمستخدم
+        # message.successful_payment.invoice_payload سيعطيك "user_premium_subscription"
+        bot.send_message(message.chat.id, "شكراً لك! تم تفعيل الاشتراك بنجاح 🌟")
