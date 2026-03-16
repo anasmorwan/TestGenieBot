a4016edcb4906cccdf1a7cc4c8e553f98bf0528e
anasmorwan <anasmorwan5@gmail.com>
2026-03-16 21:37:55 +0200

    Update file_upload.py

--- services/file_upload.py
+++ services/file_upload.py
@@ -8,13 +8,20 @@ def handle_file_upload(msg):
     chat_id = msg.chat.id
     file_id = msg.document.file_id
     file_name = msg.document.file_name
-    file_info = bot.get_file(file_id)
 
-    if is_file_size_allowed(bot, file_id):
+    if not is_file_size_allowed(bot, file_id):
         bot.send_message(chat_id, "الملف كبير جداً (الحد 5MB)")
         return
 
-    file_data = bot.download_file(file_info.file_path)
+    file_info = bot.get_file(file_id)
+    
+    try:
+        file_data = bot.download_file(file_info.file_path)
+    except Exception as e:
+        print("Download error:", e)
+        bot.send_message(chat_id, "لم أستطع قراءة الملف.")
+        return
 
     path = save_file(uid, file_name, file_data)
+
     return path
