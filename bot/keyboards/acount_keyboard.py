63556c091830c111aedf1c39fcb4e221456efb5d
anasmorwan <anasmorwan5@gmail.com>
2026-03-16 08:26:21 +0200

    Update acount_keyboard.py

--- bot/keyboards/acount_keyboard.py
+++ bot/keyboards/acount_keyboard.py
@@ -1,1 +1,10 @@
+def acount_keyboard()
+    markup = InlineKeyboardMarkup(row_width=1)
+
+    btn_status = InlineKeyboardButton("🔎 معرفة حالة حسابي", callback_data="check_account_status")
+
+    btn_upgrade = InlineKeyboardButton("🚀 ترقية الحساب", callback_data="upgrade_account")
+
+    markup.add(btn_status, btn_upgrade)
+    return markup
 
