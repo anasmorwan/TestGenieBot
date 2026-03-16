1340a8a0795b6450ae21704b66de9dd86535091e
anasmorwan <anasmorwan5@gmail.com>
2026-03-16 16:47:56 +0200

    Update flask.py

--- bot/flask.py
+++ bot/flask.py
@@ -1,3 +1,4 @@
+import flask
 # واجهة Flask للفحص
 app = Flask(__name__)
 
@@ -6,3 +7,9 @@ def register()
     def home():
         return "البوت يعمل الآن"
     
+    @app.route(f"/{BOT_TOKEN}", methods=["POST"])
+    def webhook():
+        json_str = request.get_data().decode("UTF-8")
+        update = telebot.types.Update.de_json(json_str)
+        bot.process_new_updates([update])
+        return "OK"
