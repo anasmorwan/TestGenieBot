42c4ab2d8cffbcb43f2a1e0b03d0bae5185df32a
anasmorwan <anasmorwan5@gmail.com>
2026-03-16 19:23:18 +0200

    Update menu.py

--- bot/handlers/menu.py
+++ bot/handlers/menu.py
@@ -6,6 +6,7 @@ from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 from bot.keyboards.main_menu import main_menu_keyboard
 
 def send_main_menu(chat_id, message_id=None):
+    BOT_USERNAME = bot.get_me().username
     keyboard = main_menu_keyboard()
     text = (
         "👋 مرحباً بك في TestGenie\n\n"
