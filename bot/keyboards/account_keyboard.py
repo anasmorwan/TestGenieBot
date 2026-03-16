cc9cc803617c3fdb0362da67e3f83c3e8037a3f3
anasmorwan <anasmorwan5@gmail.com>
2026-03-16 08:26:43 +0200

    Update and rename acount_keyboard.py to account_keyboard.py

--- bot/keyboards/account_keyboard.py
+++ bot/keyboards/account_keyboard.py
@@ -1,4 +1,4 @@
-def acount_keyboard()
+def account_keyboard()
     markup = InlineKeyboardMarkup(row_width=1)
 
     btn_status = InlineKeyboardButton("🔎 معرفة حالة حسابي", callback_data="check_account_status")
