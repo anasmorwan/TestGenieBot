45eb5a2e5c623de51fb56bd2aa261cf26cd9d88a
anasmorwan <anasmorwan5@gmail.com>
2026-03-16 18:23:40 +0200

    Update flask.py

--- bot/flask.py
+++ bot/flask.py
@@ -8,11 +8,15 @@ def register():
 
     @app.route("/")
     def home():
-        return "البوت يعمل الآن"
+        return "Bot is alive"
 
     @app.route(f"/{BOT_TOKEN}", methods=["POST"])
     def webhook():
-        json_str = request.get_data().decode("UTF-8")
-        update = telebot.types.Update.de_json(json_str)
+
+        json_string = request.get_data().decode("utf-8")
+
+        update = telebot.types.Update.de_json(json_string)
+
         bot.process_new_updates([update])
-        return "OK"
+
+        return "OK", 200
