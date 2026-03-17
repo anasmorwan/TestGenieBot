c48eee4101d0fad97fe66ed18c81f0be316b73fa
anasmorwan <anasmorwan5@gmail.com>
2026-03-17 17:21:22 +0200

    Update pay_local.py

--- bot/keyboards/pay_local.py
+++ bot/keyboards/pay_local.py
@@ -1,1 +1,11 @@
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+def upgrade_options_keyboard():
+
+    reply_markup = InlineKeyboardMarkup(row_width=1)
+  
+    reply_markup.add(
+        InlineKeyboardButton("💬 تواصل معنا", url="https://t.me/anasM2002"),
+        InlineKeyboardButton("🔙 رجوع", callback_data="upgrade_back")
+    )
+    return reply_markup
