c91a45df67c6c6e6f70e3ff7590ffc00024c962d
anasmorwan <anasmorwan5@gmail.com>
2026-03-17 16:41:33 +0200

    Create upgrade_options.py

---
+++ bot/keyboards/upgrade_options.py
@@ -0,0 +1,10 @@
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+def upgrade_options_keyboard():
+
+    upgrade_options_keyboard = InlineKeyboardMarkup(row_width=1)
+    upgrade_options_keyboard.add(
+        InlineKeyboardButton("🌟 الدفع بالنجوم", callback_data="pay_stars"),
+        InlineKeyboardButton("🏦 الدفع المحلي (السودان)", callback_data="pay_local"),
+        InlineKeyboardButton("📩 تواصل معي للدفع", url="https://t.me/YourContact")
+    )
