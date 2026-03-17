5263acca0e549fd1037959de57ebef05b2dacb15
anasmorwan <anasmorwan5@gmail.com>
2026-03-17 21:37:10 +0200

    Update upgrade_keyboard.py

--- bot/keyboards/upgrade_keyboard.py
+++ bot/keyboards/upgrade_keyboard.py
@@ -4,7 +4,7 @@ def upgrade_keyboard():
     # إعداد لوحة الأزرار
     keyboard = keyboard = InlineKeyboardMarkup(row_width=1)
     buttons = [
-        InlineKeyboardButton("💳 تفعيل TestGenie Pro", callback_data="buy_subscription"),
+        InlineKeyboardButton("💳 تفعيل TestGenie Pro", callback_data="plans"),
         InlineKeyboardButton("🔍 معرفة المميزات", callback_data="premium_info"),
         InlineKeyboardButton("🏠 العودة للقائمة الرئيسية", callback_data="main_menu")
     ]
