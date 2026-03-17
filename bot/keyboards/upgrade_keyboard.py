359430ecc917cb0f0119d202c228d7f8e76ee176
anasmorwan <anasmorwan5@gmail.com>
2026-03-17 13:45:59 +0200

    Update upgrade_keyboard.py

--- bot/keyboards/upgrade_keyboard.py
+++ bot/keyboards/upgrade_keyboard.py
@@ -1,10 +1,12 @@
-from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 def upgrade_keyboard():
     # إعداد لوحة الأزرار
-    keyboard = InlineKeyboardMarkup([
-        [InlineKeyboardButton("💳 شراء الاشتراك", callback_data="buy_subscription")],
-        [InlineKeyboardButton("🔙 رجوع", callback_data="go_account_settings")]
-    ])
+    keyboard = keyboard = InlineKeyboardMarkup(row_width=1)
+    buttons = [
+        InlineKeyboardButton("💳 شراء الاشتراك", callback_data="buy_subscription"),
+        InlineKeyboardButton("🔙 رجوع", callback_data="go_account_settings")
+    ]
+    keyboard.add(*buttons)
     return keyboard
 
