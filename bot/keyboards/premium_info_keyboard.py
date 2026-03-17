1ea83e37605c792c31dd54d86d7a5e9fe4aa8635
anasmorwan <anasmorwan5@gmail.com>
2026-03-17 19:19:37 +0200

    Update premium_info_keyboard.py

--- bot/keyboards/premium_info_keyboard.py
+++ bot/keyboards/premium_info_keyboard.py
@@ -1,1 +1,11 @@
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+def upgrade_keyboard():
+    # إعداد لوحة الأزرار
+    keyboard = keyboard = InlineKeyboardMarkup(row_width=1)
+    buttons = [
+        InlineKeyboardButton("💳 تفعيل TestGenie Pro", callback_data="buy_subscription"),
+        InlineKeyboardButton("🏠 العودة للقائمة الرئيسية", callback_data="main_menu")
+    ]
+    keyboard.add(*buttons)
+    return keyboard
