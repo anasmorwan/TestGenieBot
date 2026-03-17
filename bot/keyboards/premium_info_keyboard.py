9d7cbd2652510835c0b8f00d7b7205cabdd3c38d
anasmorwan <anasmorwan5@gmail.com>
2026-03-17 21:37:48 +0200

    Update premium_info_keyboard.py

--- bot/keyboards/premium_info_keyboard.py
+++ bot/keyboards/premium_info_keyboard.py
@@ -4,7 +4,7 @@ def premium_info_keyboard():
     # إعداد لوحة الأزرار
     keyboard = keyboard = InlineKeyboardMarkup(row_width=1)
     buttons = [
-        InlineKeyboardButton("💳 تفعيل TestGenie Pro", callback_data="buy_subscription"),
+        InlineKeyboardButton("💳 تفعيل TestGenie Pro", callback_data="plans"),
         InlineKeyboardButton("🏠 العودة للقائمة الرئيسية", callback_data="main_menu")
     ]
     keyboard.add(*buttons)
