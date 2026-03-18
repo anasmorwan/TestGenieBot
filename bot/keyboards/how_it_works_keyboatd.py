a779473c5d6611ee68fca8670ebf8929dc5298da
anasmorwan <anasmorwan5@gmail.com>
2026-03-18 16:57:28 +0200

    Update how_it_works_keyboatd.py

--- bot/keyboards/how_it_works_keyboatd.py
+++ bot/keyboards/how_it_works_keyboatd.py
@@ -1,3 +1,11 @@
-web_app_button = InlineKeyboardButton(
-        text="📖 قراءة فورية", 
-        web_app=WebAppInfo(url="https://example.com/page.html") 
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
+
+def how_it_works_keyboatd():
+    keyboard = InlineKeyboardMarkup()
+    web_app_button = InlineKeyboardButton(
+    text="📖 قراءة فورية",
+    web_app=WebAppInfo(url="https://example.com/page.html") 
+    )
+    keyboard.add(web_app_button)
+    keyboard.add(InlineKeyboardButton("
+    return keyboard
