f26b35b17197d58b68b0b181aae945f62033ffa2
anasmorwan <anasmorwan5@gmail.com>
2026-03-18 17:38:12 +0200

    Update poll_service.py

--- services/poll_service.py
+++ services/poll_service.py
@@ -1,17 +1,24 @@
 
 
 def send_quiz_poll(bot, chat_id, question):
+    try:
+        if question.correct_index >= len(question.options):
+            print("Invalid correct index")
+            return
+        poll = bot.send_poll(
+            chat_id=chat_id,
+            question=str(question.question)[:300],
+            options=[str(opt) for opt in question.options if opt],
+            type="quiz",
+            correct_option_id=int(question.correct_index),
+            explanation=str(question.explanation or "")[:200],
+            is_anonymous=False,
+            open_period=30
+        )
+        return poll.message_id
 
-    poll = bot.send_poll(
-        chat_id=chat_id,
-        question=question.question,
-        options=question.options,
-        type="quiz",
-        correct_option_id=question.correct_index,
-        explanation=question.explanation,
-        is_anonymous=False,
-        open_period=30
-    )
-
-    return poll.message_id
-
+    except Exception as e:
+        import logging
+        logging.exception("SEND POLL FAILED")
+        print("FAILED DATA:", question.__dict__)
+        return None
