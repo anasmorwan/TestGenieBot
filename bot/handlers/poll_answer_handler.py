ed558d067eb72871aa38999f6d535225c826acc6
anasmorwan <anasmorwan5@gmail.com>
2026-03-18 19:58:53 +0200

    Update poll_answer_handler.py

--- bot/handlers/poll_answer_handler.py
+++ bot/handlers/poll_answer_handler.py
@@ -6,9 +6,11 @@ def register(bot):
     @bot.poll_answer_handler()
     def handle_poll_answer(poll_answer):
         print("POLL ANSWER RECEIVED:", poll_answer)
-
+        
         user_id = poll_answer.user.id
         chat_id = quiz_manager.poll_map.get(poll_answer.poll_id)
+        print("RESOLVED CHAT ID:", chat_id)
+        print("POLL MAP:", quiz_manager.poll_map)
         selected_option = poll_answer.option_ids[0] if poll_answer.option_ids else None
 
         # 👇 هنا تنادي الانتقال للسؤال التالي
