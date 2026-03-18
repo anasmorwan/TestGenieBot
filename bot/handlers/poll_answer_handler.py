2b2432bb5204e1214b42f77f15a904915adfddfe
anasmorwan <anasmorwan5@gmail.com>
2026-03-18 18:55:15 +0200

    Update poll_answer_handler.py

--- bot/handlers/poll_answer_handler.py
+++ bot/handlers/poll_answer_handler.py
@@ -1,9 +1,13 @@
-@bot.poll_answer_handler()
-def handle_poll_answer(poll_answer):
-    print("POLL ANSWER RECEIVED:", poll_answer)
 
-    user_id = poll_answer.user.id
-    selected_option = poll_answer.option_ids[0] if poll_answer.option_ids else None
+def register(bot):
+    
+    @bot.poll_answer_handler()
+    def handle_poll_answer(poll_answer):
+        print("POLL ANSWER RECEIVED:", poll_answer)
 
-    # 👇 هنا تنادي الانتقال للسؤال التالي
-    quiz_manager.handle_answer(user_id, selected_option, bot)
+        user_id = poll_answer.user.id
+        chat_id = quiz_manager.poll_map.get(poll_answer.poll_id)
+        selected_option = poll_answer.option_ids[0] if poll_answer.option_ids else None
+
+        # 👇 هنا تنادي الانتقال للسؤال التالي
+        quiz_manager.handle_answer(user_id, selected_option, bot)
