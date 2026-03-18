717abf2344cf83f90fdc314979f24683578c96a1
anasmorwan <anasmorwan5@gmail.com>
2026-03-18 17:14:38 +0200

    Update how_it_works_keyboatd.py

--- bot/keyboards/how_it_works_keyboatd.py
+++ bot/keyboards/how_it_works_keyboatd.py
@@ -4,8 +4,8 @@ def how_it_works_keyboatd():
     keyboard = InlineKeyboardMarkup(row_width=1)
 
     web_app_button = InlineKeyboardButton(
-    text="⚡ قراءة فورية",
-    web_app=WebAppInfo(url="https://example.com/page.html") 
+    text="⚡ كيف يعمل؟",
+    url="https://example.com/how-it-works"
     )
     keyboard.add(web_app_button)
     btn_back = InlineKeyboardButton("🏠 العودة للقائمة الرئيسية", callback_data="main_menu")
