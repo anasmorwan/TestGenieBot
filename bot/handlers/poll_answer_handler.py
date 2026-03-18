044621a36f85b5b494d5ad1e3c8aa0abbef20f34
anasmorwan <anasmorwan5@gmail.com>
2026-03-18 18:52:52 +0200

    Create poll_answer_handler.py

---
+++ bot/handlers/poll_answer_handler.py
@@ -0,0 +1,9 @@
+@bot.poll_answer_handler()
+def handle_poll_answer(poll_answer):
+    print("POLL ANSWER RECEIVED:", poll_answer)
+
+    user_id = poll_answer.user.id
+    selected_option = poll_answer.option_ids[0] if poll_answer.option_ids else None
+
+    # 👇 هنا تنادي الانتقال للسؤال التالي
+    quiz_manager.handle_answer(user_id, selected_option, bot)
