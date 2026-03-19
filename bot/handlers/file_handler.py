5559196179ae971f419caf538ccbb120b7e7bb84
anasmorwan <anasmorwan5@gmail.com>
2026-03-20 00:27:50 +0200

    Update file_handler.py

--- bot/handlers/file_handler.py
+++ bot/handlers/file_handler.py
@@ -34,14 +34,12 @@ def register(bot):
 
         try:
 
-            allowed, reason = can_generate(user_id)
+            allowed, info = can_generate(user_id)
 
-
-        
             if not allowed:
-                if reason == "limit_reached":
-                    show_referral_message(bot, chat_id)
-
+                show_referral_message(bot, chat_id)
+                return  # ❗ هذا هو المفتاح
+                
             # 👇 استهلك محاولة
             consume_quiz(user_id)
             # 👇 تحقق هل هذا مستخدم جديد تمت دعوته
