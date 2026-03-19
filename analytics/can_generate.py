9755edfd7c37d76801b348c77365e953462ae040
anasmorwan <anasmorwan5@gmail.com>
2026-03-19 14:39:27 +0200

    Update can_generate.py

--- analytics/can_generate.py
+++ analytics/can_generate.py
@@ -1,8 +1,28 @@
 from storage.sqlite_db import get_connection
 
+conn = get_connection()
+    c = conn.cursor()
+
+    # تأكد أنه ليس مكرر
+    c.execute("""
+        SELECT 1 FROM referrals 
+        WHERE referrer_id=? AND referred_id=?
+    """, (referrer_id, referred_id))
+
+    if c.fetchone():
+        return
+
+    c.execute("""
+        INSERT INTO referrals (referrer_id, referred_id)
+        VALUES (?, ?)
+    """, (referrer_id, referred_id))
+
+    conn.commit()
+    conn.close()
+
 def check_status(user_id):
     conn = get_connection()
-    curr = con.currcor
+    c = conn.cursor()
 
 
 def can_generate(uid):
