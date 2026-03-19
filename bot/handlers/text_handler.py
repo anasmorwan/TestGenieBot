e8db4a9c0e39f6dee9a957313990bad1599adb11
anasmorwan <anasmorwan5@gmail.com>
2026-03-19 15:38:33 +0200

    Update text_handler.py

--- bot/handlers/text_handler.py
+++ bot/handlers/text_handler.py
@@ -17,7 +17,7 @@ def register(bot):
 
         
         if not can generate(user_id):
-            show_referral_message(bot)
+            show_referral_message(bot, chat_id)
 
         # 👇 استهلك محاولة
         consume_quiz(user_id)
