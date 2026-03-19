a5c4746bf92819c111b773de17ae2aa543635b30
anasmorwan <anasmorwan5@gmail.com>
2026-03-19 17:25:48 +0200

    Update usage.py

--- services/usage.py
+++ services/usage.py
@@ -2,20 +2,19 @@ from storage.sqlite_db import get_connection
 
 
 
-"""
-def consume_quiz(user_id):
-    conn = get_connection()
-    c = conn.cursor()
+# def consume_quiz(user_id):
+#    conn = get_connection()
+#    c = conn.cursor()
 
-    c.execute("""
-        UPDATE users 
-        SET free_quizzes = free_quizzes - 1 
-        WHERE user_id=? AND free_quizzes > 0
-    """, (user_id,))
+#    c.execute("""
+#        UPDATE users 
+#        SET free_quizzes = free_quizzes - 1 
+#        WHERE user_id=? AND free_quizzes > 0
+#    """, (user_id,))
 
-    conn.commit()
-    conn.close()
-"""
+#    conn.commit()
+#    conn.close()
+
 
 def consume_quiz(user_id):
     conn = get_connection()
@@ -31,21 +30,21 @@ def consume_quiz(user_id):
     conn.close()
 
 
-"""
-def can_generate(user_id):
-    conn = get_connection()
-    c = conn.cursor()
 
-    c.execute("SELECT free_quizzes FROM users WHERE user_id=?", (user_id,))
-    row = c.fetchone()
+# def can_generate(user_id):
+ #   conn = get_connection()
+#    c = conn.cursor()
+#
+#    c.execute("SELECT free_quizzes FROM users WHERE user_id=?", (user_id,))
+#    row = c.fetchone()
 
-    conn.close()
+#    conn.close()
 
-    if not row:
-        return False
+#    if not row:
+#        return False
 
-    return row[0] > 0
-"""
+#    return row[0] > 0
+
 def can_generate(user_id):
     sub = get_subscription(user_id)
 
