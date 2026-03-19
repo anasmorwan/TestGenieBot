84a0a4677c29f16e6f4de84c0db7d47b23e9fd55
anasmorwan <anasmorwan5@gmail.com>
2026-03-19 14:37:17 +0200

    Update can_generate.py

--- analytics/can_generate.py
+++ analytics/can_generate.py
@@ -1,4 +1,11 @@
+from storage.sqlite_db import get_connection
+
+def check_status(user_id):
+    conn = get_connection()
+    curr = con.currcor
 
 
 def can_generate(uid):
+    if user.free_quizzes <= 0:
+        return False
     return True
