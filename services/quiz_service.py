dacec159608985ff021f066e24a93b308bf5e4d8
anasmorwan <anasmorwan5@gmail.com>
2026-03-20 14:18:10 +0200

    Update quiz_service.py

--- services/quiz_service.py
+++ services/quiz_service.py
@@ -2,28 +2,35 @@
 from ai.llm_client import generate_smart_response
 from ai.prompts import build_quiz_prompt
 from utils.json_utils import extract_json_from_string, extract_json_objects_safely
+from ai.prompts import pro_quiz_generator
+from services.usage import is_paid_user_active
 
 # quiz_service.py
-
 
 
 # ======================================
 num_quizzes = 5
 def generate_quizzes_from_text(content, user_id, user_instruction=None, num_quizzes=5):
-    prompt = build_quiz_prompt(content, num_quizzes, user_instruction=user_instruction)
+    if is_paid_user_active(user_id):
+        response = pro_quiz_generator(content, num_questions=5)
+        return response
 
-    response = generate_smart_response(prompt)
-    print("RAW RESPONSE:\n", response[:1000])
+    else:
+        
+        prompt = build_quiz_prompt(content, num_quizzes, user_instruction=user_instruction)
 
-    # استخدام الدالة الجديدة للمعالجة الآمنة
-    quizzes = extract_json_objects_safely(response)
+        response = generate_smart_response(prompt)
+        print("RAW RESPONSE:\n", response[:1000])
 
-    print("PARSED QUIZZES:", type(quizzes), len(quizzes) if isinstance(quizzes, list) else "invalid")
+        # استخدام الدالة الجديدة للمعالجة الآمنة
+        quizzes = extract_json_objects_safely(response)
 
-    if not isinstance(quizzes, list):
-        return []
+        print("PARSED QUIZZES:", type(quizzes), len(quizzes) if isinstance(quizzes, list) else "invalid")
 
-    # تحديد العدد المطلوب فقط
-    quizzes = quizzes[:num_quizzes]
+        if not isinstance(quizzes, list):
+            return []
 
-    return quizzes
+        # تحديد العدد المطلوب فقط
+        quizzes = quizzes[:num_quizzes]
+
+        return quizzes
