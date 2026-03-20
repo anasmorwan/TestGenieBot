2320582817c42778d364eae593619218a380c472
anasmorwan <anasmorwan5@gmail.com>
2026-03-20 03:28:30 +0200

    Update referral_keyboard.py

--- bot/keyboards/referral_keyboard.py
+++ bot/keyboards/referral_keyboard.py
@@ -2,11 +2,12 @@ from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 def referral_keyboard(user_id):
     ref_code = user_id
+    invite_link = f"https://t.me/testprog123bot?start=ref_{ref_code}"
     referal_btns = InlineKeyboardMarkup(row_width=1)
     
     referal_btns.add(
     InlineKeyboardButton(text="👥 دعوة صديق", url=f"https://t.me/share/url?url=https://t.me/testprog123bot?start=ref_{ref_code}&text=🔥 جرب هذا البوت! يحول أي ملف لاختبار تفاعلي خلال ثواني"), 
-    InlineKeyboardButton(text="📋 نسخ رابطي", callback_data="copy_invite")
+    InlineKeyboardButton(text="📋 نسخ رابطي", copy_text=invite_link)
     )
     return referal_btns
     
