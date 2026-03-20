0f925233ee380d34204c742aece620805095934d
anasmorwan <anasmorwan5@gmail.com>
2026-03-20 02:50:35 +0200

    Update account_status_keyboard.py

--- bot/keyboards/account_status_keyboard.py
+++ bot/keyboards/account_status_keyboard.py
@@ -1,1 +1,14 @@
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+
+def account_status_keyboard():
+    markup = InlineKeyboardMarkup(row_width=1)
+
+
+    btn_upgrade = InlineKeyboardButton("🚀 ترقية الحساب", callback_data="upgrade_account")
+    btn_back = InlineKeyboardButton("🏠 العودة للقائمة الرئيسية", callback_data="main_menu")
+
+
+    markup.add(btn_upgrade, btn_back)
+    return markup
 
