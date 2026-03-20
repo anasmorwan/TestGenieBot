4818d23fabad1eedfb6d1f79c59a5abf6334d0bf
anasmorwan <anasmorwan5@gmail.com>
2026-03-20 03:47:15 +0200

    Update referral_keyboard.py

--- bot/keyboards/referral_keyboard.py
+++ bot/keyboards/referral_keyboard.py
@@ -7,7 +7,7 @@ def referral_keyboard(user_id):
     
     referal_btns.add(
     InlineKeyboardButton(text="👥 دعوة صديق", url=f"https://t.me/share/url?url=https://t.me/testprog123bot?start=ref_{ref_code}&text=🔥 جرب هذا البوت! يحول أي ملف لاختبار تفاعلي خلال ثواني"), 
-    InlineKeyboardButton(text="📋 نسخ رابطي", copy_text=CopyTextButton(invite_link))
+    InlineKeyboardButton(text="📋 نسخ رابطي", callback="copylink")
     )
     
     return referal_btns
