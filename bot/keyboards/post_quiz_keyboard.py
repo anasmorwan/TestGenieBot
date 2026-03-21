bf801c1a20ddb50b631e8bcbed1e03bd3c5fafc0
anasmorwan <anasmorwan5@gmail.com>
2026-03-21 17:09:12 +0200

    Create post_quiz_keyboard.py

---
+++ bot/keyboards/post_quiz_keyboard.py
@@ -0,0 +1,15 @@
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+
+def account_status_keyboard(user_id):
+    markup = InlineKeyboardMarkup(row_width=1)
+    ref_code = user_id
+
+
+    btn_upgrade = InlineKeyboardButton("🚀 ترقية الحساب", callback_data="upgrade_account")
+    btn_back = InlineKeyboardButton("🏠 العودة للقائمة الرئيسية", callback_data="main_menu")
+    btn_refer = InlineKeyboardButton(text="👥 دعوة صديق", url=f"https://t.me/share/url?url=https://t.me/testprog123bot?start=ref_{ref_code}&text=🔥 جرب هذا البوت! يحول أي ملف لاختبار تفاعلي خلال ثواني") 
+
+
+    markup.add(btn_refer, btn_upgrade, btn_back)
+    return markup
