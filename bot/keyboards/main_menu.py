58b04bb3cbddf7ca1ca5421a0cde6e551925a2df
anasmorwan <anasmorwan5@gmail.com>
2026-03-21 11:20:54 +0200

    Update main_menu.py

--- bot/keyboards/main_menu.py
+++ bot/keyboards/main_menu.py
@@ -7,20 +7,5 @@ def main_menu_keyboard(bot_username):
     keyboard.row(
         InlineKeyboardButton("📝 نص مباشر", callback_data="input_text"),
         InlineKeyboardButton("📎 إرسال ملف", callback_data="go_generate")
-    )
-
-    keyboard.row(
-        InlineKeyboardButton("⚡ اختبار عشوائي", callback_data="quick_quiz"),
-        InlineKeyboardButton("📖 كيف يعمل؟", callback_data="how_it_works")
-    )
-
-    keyboard.row(
-        InlineKeyboardButton("👤 حسابي", callback_data="go_account_settings"),
-        InlineKeyboardButton("🚀 TestGenie Pro", callback_data="upgrade_account")
-    )
-
-    keyboard.add(
-        InlineKeyboardButton("➕ أضفني إلى مجموعة", url=f"https://t.me/{bot_username}?startgroup=true")
-    )
 
     return keyboard
