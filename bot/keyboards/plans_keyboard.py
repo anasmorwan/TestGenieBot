db2d270e362e307ae9ca545c34bfa2a36004c972
anasmorwan <anasmorwan5@gmail.com>
2026-03-21 14:43:58 +0200

    Update plans_keyboard.py

--- bot/keyboards/plans_keyboard.py
+++ bot/keyboards/plans_keyboard.py
@@ -4,9 +4,8 @@ def paid_plans_keyboard():
 
     upgrade_options_keyboard = InlineKeyboardMarkup(row_width=1)
     upgrade_options_keyboard.add(
-        InlineKeyboardButton("🟢 خطة Pro", callback_data="buy_subscription1"),
-        InlineKeyboardButton("🔵 خطة Pro+", callback_data="buy_subscription2"),
-        InlineKeyboardButton("📩 تواصل معي", url="https://t.me/anasM2002"),
-        InlineKeyboardButton("🏠 العودة للقائمة الرئيسية", callback_data="main_menu")
+        InlineKeyboardButton("🔥 اشتراك Pro", callback_data="buy_subscription1"),
+        InlineKeyboardButton("⭐ اشتراك Pro+", callback_data="buy_subscription2"),
+        InlineKeyboardButton("⬅️ رجوع", callback_data="main_menu")
     )
     return upgrade_options_keyboard
