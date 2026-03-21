e0e05cf791f22292d79bb98f8a8ad2f0b7762502
anasmorwan <anasmorwan5@gmail.com>
2026-03-21 11:26:55 +0200

    Update main_menu.py

--- bot/keyboards/main_menu.py
+++ bot/keyboards/main_menu.py
@@ -4,8 +4,14 @@ from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 def main_menu_keyboard(bot_username):
     keyboard = InlineKeyboardMarkup(row_width=1)
 
-    keyboard.row(
-        InlineKeyboardButton("📝 نص مباشر", callback_data="input_text"),
-        InlineKeyboardButton("📎 إرسال ملف", callback_data="go_generate")
-
+    buttons = [
+        InlineKeyboardButton("📄 إرسل ملف", callback_data="go_generate"),
+        InlineKeyboardButton("✍️ إرسل نص", callback_data="input_text"),
+        InlineKeyboardButton("⚙️ المزيد", callback_data="more_options")
+    ]
+    keyboard.add(buttons)
     return keyboard
+
+
+ 
+    
