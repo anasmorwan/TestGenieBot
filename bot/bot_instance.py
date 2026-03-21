998044807b236e9e10d256b889c284ba3c21eb9b
anasmorwan <anasmorwan5@gmail.com>
2026-03-21 22:19:58 +0200

    Update bot_instance.py

--- bot/bot_instance.py
+++ bot/bot_instance.py
@@ -11,7 +11,7 @@ WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 mybot = telebot.TeleBot(BOT_TOKEN)
 
 def set_webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url=f"{WEBHOOK_URL}/{BOT_TOKEN}")
+    mybot.remove_webhook()
+    mybot.set_webhook(url=f"{WEBHOOK_URL}/{BOT_TOKEN}")
     logging.info(f"🌍 Webhook set at {WEBHOOK_URL}/{BOT_TOKEN}")
     
