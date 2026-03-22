abdfdc8e6aac6204deb07f76e986c37824be4599
anasmorwan <anasmorwan5@gmail.com>
2026-03-22 18:00:31 +0200

    Create metrics.py

---
+++ analytics/metrics.py
@@ -0,0 +1,81 @@
+from storage.sqlite_db import get_connection
+
+
+
+def get_total_users():
+    conn = get_connection()
+    c = conn.cursor()
+
+    c.execute("SELECT COUNT(*) FROM users")
+    total = c.fetchone()[0]
+
+    conn.close()
+    return total
+
+
+def get_active_today():
+    conn = get_connection()
+    c = conn.cursor()
+
+    today = datetime.utcnow().date().isoformat()
+
+    c.execute("""
+    SELECT COUNT(*) FROM users
+    WHERE DATE(last_reset) = ?
+    """, (today,))
+
+    count = c.fetchone()[0]
+
+    conn.close()
+    return count
+
+
+def get_users_hit_limit():
+    conn = get_connection()
+    c = conn.cursor()
+
+    c.execute("""
+    SELECT COUNT(*) FROM users
+    WHERE used_today >= daily_limit
+    """)
+
+    count = c.fetchone()[0]
+
+    conn.close()
+    return count
+
+
+def get_paid_users():
+    conn = get_connection()
+    c = conn.cursor()
+
+    c.execute("""
+    SELECT COUNT(*) FROM subscriptions
+    WHERE plan IN ('pro', 'pro_plus')
+    """)
+
+    count = c.fetchone()[0]
+
+    conn.close()
+    return count
+
+def get_total_referrals():
+    conn = get_connection()
+    c = conn.cursor()
+
+    c.execute("SELECT COUNT(*) FROM referrals")
+    count = c.fetchone()[0]
+
+    conn.close()
+    return count
+
+
+
+def get_metrics():
+    return {
+        "users": get_total_users(),
+        "active_today": get_active_today(),
+        "hit_limit": get_users_hit_limit(),
+        "paid": get_paid_users(),
+        "referrals": get_total_referrals()
+    }
