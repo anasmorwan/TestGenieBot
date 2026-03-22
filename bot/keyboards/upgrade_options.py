bb544588e08f5ace8d0d5f1885a3fe4ffdd2147f
anasmorwan <anasmorwan5@gmail.com>
2026-03-22 20:49:41 +0200

    Update upgrade_options.py

--- bot/keyboards/upgrade_options.py
+++ bot/keyboards/upgrade_options.py
@@ -5,7 +5,7 @@ def upgrade_options_keyboard():
     upgrade_options_keyboard = InlineKeyboardMarkup(row_width=1)
     upgrade_options_keyboard.add(
         InlineKeyboardButton("🌟 الدفع بالنجوم", callback_data="pay_stars"),
-        InlineKeyboardButton("🅿️ paypal", url="https://paypal.me/hosam2025"),
+        InlineKeyboardButton("🅿️ paypal", url="paypal"),
         InlineKeyboardButton("🏦 الدفع المحلي (السودان)", callback_data="pay_local"),
         InlineKeyboardButton("📩 تواصل معي للدفع", url="https://t.me/anasM2002"),
         InlineKeyboardButton("🔙 رجوع", callback_data="plans")
