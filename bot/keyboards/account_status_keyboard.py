75cfd3668a44a2a1379a37790309c4a9ba2d5090
anasmorwan <anasmorwan5@gmail.com>
2026-03-22 16:28:19 +0200

    Update account_status_keyboard.py

--- bot/keyboards/account_status_keyboard.py
+++ bot/keyboards/account_status_keyboard.py
@@ -5,7 +5,7 @@ def account_status_keyboard(user_id):
     markup = InlineKeyboardMarkup(row_width=1)
     ref_code = user_id
 
-    btn_update = InlineKeyboardButton(text="🔁 تحديث", callback_data="check_account_status")
+    btn_update = InlineKeyboardButton(text="🔁 تحديث", callback_data="update_account_status")
     btn_refer = InlineKeyboardButton(text="👥 دعوة صديق", url=f"https://t.me/share/url?url=https://t.me/testprog123bot?start=ref_{ref_code}&text=🔥 جرب هذا البوت! يحول أي ملف لاختبار تفاعلي خلال ثواني")  
     btn_upgrade = InlineKeyboardButton("🚀 ترقية الحساب", callback_data="upgrade_account")
     btn_back = InlineKeyboardButton("🏠 العودة للقائمة الرئيسية", callback_data="main_menu")
