5dc220508a040dcc8a18a99ad86831193c46cc87
anasmorwan <anasmorwan5@gmail.com>
2026-03-23 23:04:47 +0200

    Update quiz_session_service.py

--- services/quiz_session_service.py
+++ services/quiz_session_service.py
@@ -8,7 +8,7 @@ import threading
 from services.usage import is_paid_user_active
 from storage.messages import get_message
 # from bot.keyboards.upsell_keyboard import quiz_number_limit_upsell, tracking_upsell_keyboard
-from storage.quiz_attempts import log_quiz_attempt, get_quiz_stats, build_quiz_viral_message, format_usernames, get_quiz_user_ids
+from storage.quiz_attempts import log_quiz_attempt, get_quiz_stats, build_quiz_viral_message, get_quiz_creator, format_usernames, get_quiz_user_ids
 from analytics.shared_quiz_analytics import get_hardest_question, get_success_rate, build_advanced_stats_message
 from bot.keyboards.quiz_buttons import share_quiz_button
 import random
@@ -166,23 +166,31 @@ class QuizManager:
         elif shared:
             
             if quiz_code:
+                creator_id = get_quiz_creator(quiz_code)
                 log_quiz_attempt(chat_id, quiz_code, score, total)
                 stats = get_quiz_stats(quiz_code)
 
                 # if stats["users"] >= 3 and stats["completed"] < 5:
+
+                
                 if stats["users"] >= 3:
+                    
                     user_ids = get_quiz_user_ids(quiz_code)
                     names = format_usernames(bot, user_ids)
 
                     message = build_quiz_viral_message(stats, names)
                     keyboard = tracking_upsell_keyboard()
-                    bot.send_message(chat_id=chat_id, text=message, reply_markup=keyboard)
+                    if user_id == creator_id:
+                    bot.send_message(chat_id=creator_id, text=message, reply_markup=keyboard)
 
      
                 elif stats["users"] >= 3 and stats["completed"] >= 5:
                     hardest = get_hardest_question(quiz_code)
                     success = get_success_rate(quiz_code)
                     message = build_advanced_stats_message(stats, hardest, success)
+                    keyboard = tracking_upsell_keyboard()
+                    bot.send_message(chat_id=creator_id, text=message, reply_markup=keyboard)
+
                     waiting_msg = bot.send_message(chat_id, "⏳ جارٍ تحليل النتائج...")
                     
                     wait_time = random.uniform(1, 3)
@@ -196,7 +204,7 @@ class QuizManager:
                             text="🎉"
                         )
                         time.sleep(2)
-                    keyboard = tracking_upsell_keyboard()
+                    
                     bot.edit_message_text(chat_id, message_id=waiting_msg.message_id, text=message, reply_markup=keyboard, parse_mode="HTML")
 
             
