2c71f0a97c6a393a333a2e41c29c34666f525d39
anasmorwan <anasmorwan5@gmail.com>
2026-03-24 00:33:41 +0200

    Update quiz_session_service.py

--- services/quiz_session_service.py
+++ services/quiz_session_service.py
@@ -7,7 +7,7 @@ from models.quiz import QuizQuestion
 import threading
 from services.usage import is_paid_user_active
 from storage.messages import get_message
-# from bot.keyboards.upsell_keyboard import quiz_number_limit_upsell, tracking_upsell_keyboard
+from bot.keyboards.upsell_keyboard import quiz_number_limit_upsell, tracking_upsell_keyboard
 from storage.quiz_attempts import log_quiz_attempt, get_quiz_stats, build_quiz_viral_message, get_quiz_creator, format_usernames, get_quiz_user_ids
 from analytics.shared_quiz_analytics import get_hardest_question, get_success_rate, build_advanced_stats_message
 from bot.keyboards.quiz_buttons import share_quiz_button
