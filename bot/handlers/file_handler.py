91e8491e754f27df9aa24faf7d6f85b8a2e1507c
anasmorwan <anasmorwan5@gmail.com>
2026-03-23 20:57:20 +0200

    Update file_handler.py

--- bot/handlers/file_handler.py
+++ bot/handlers/file_handler.py
@@ -58,7 +58,7 @@ def register(bot):
         
         try:
             waiting_msg = bot.send_message(chat_id=chat_id, text=get_message("FILE_QUIZ"))
-            path = handle_file_upload(msg)
+            path, filename = handle_file_upload(msg)
 
         except Exception as e:
             print("FILE UPLOAD ERROR:", e, flush=True)
