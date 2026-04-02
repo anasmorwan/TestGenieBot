98d2e0aa6f71ae606d1522dd35431d0eb4518611
anasmorwan <anasmorwan5@gmail.com>
2026-04-02 17:57:02 +0200

    Update actions_keyboard.py

--- bot/keyboards/actions_keyboard.py
+++ bot/keyboards/actions_keyboard.py
@@ -4,21 +4,21 @@ from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 def send_poll_keyboard(text, poll_code):
     markup = InlineKeyboardMarkup(row_width=1)
 
-    btn_status = InlineKeyboardButton("🚀 نشر في القناة", callback_data=f"post_poll:{poll_code}")
-    btn_upgrade = InlineKeyboardButton("🔄 إعادة توليد", callback_data=f"regenerate:{text}")
+    btn_post = InlineKeyboardButton("🚀 نشر في القناة", callback_data=f"post_poll:{poll_code}")
+    btn_regenerate = InlineKeyboardButton("🔄 إعادة توليد", callback_data=f"regenerate:{text}")
 
     
 
-    markup.add(btn_status, btn_upgrade, btn_back)
+    markup.add(btn_post, btn_regenerate)
     return markup
 
 def escape_action_keyboard():
     markup = InlineKeyboardMarkup(row_width=1)
 
     
-    btn_upgrade = InlineKeyboardButton("× إلغاء", callback_data=f"cancel")
+    btn_cancel = InlineKeyboardButton("× إلغاء", callback_data=f"cancel")
 
     
 
-    markup.add(btn_status, btn_upgrade, btn_back)
+    markup.add(btn_cancel)
     return markup
