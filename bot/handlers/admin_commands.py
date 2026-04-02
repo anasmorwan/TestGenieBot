dff1b0f0666f1019fb7d413854623c206acb056e
anasmorwan <anasmorwan5@gmail.com>
2026-04-02 08:54:21 +0200

    Update admin_commands.py

--- bot/handlers/admin_commands.py
+++ bot/handlers/admin_commands.py
@@ -2,9 +2,6 @@ import os
 from services.usage import activate_subscription_manual, reset_or_set_daily_usage, get_user_full_info
 from analytics.metrics import get_metrics
 from services.backup_service import backup_all
-from bot.keyboards.more_options_keyboard import more_options_keyboard
-from bot.keyboards.main_menu import main_menu_keyboard
-from storage.messages import get_message
 
 
 ADMIN_ID = int(os.getenv("ADMIN_ID"))
@@ -120,42 +117,4 @@ def register(bot):
         except Exception as e:
             bot.reply_to(msg, f"❌ الخطأ: {str(e)}")
 
-
-
-    @bot.message_handler(commands=["menu"])
-    def user_info(msg):
-        try:
-            user_id = msg.from_user.id
-            chat_id = msg.chat.id
-            
-            bot_username = bot.get_me().username
-    
-            keyboard = more_options_keyboard(bot_username)
-            
-            
-            base_text = get_message("BASE_TEXT")
-            menu_text = get_message("MORE")
-    
-            # النص المتغير (التحية أو مقدمة مخصصة)
-            welcome_new_user = "<b>👋 مرحباً بك في TestGenie</b>\n\n"
-            welcome_returning_user = "<b>👋 مرحباً بك مجددًا في TestGenie</b>\n\nما الذي ترغب في القيام به اليوم؟\n\n"
-        
-            #if is_user_exist(chat_id):
-            #text = ux_text          
-        
-        
-            #else:    
-            #   text = ux_text
-
-            text = menu_text
-                
-    
-            bot.send_message(chat_id,
-            text=text,
-            reply_markup=keyboard,
-            parse_mode="HTML")
-
-        except Exception as e:
-            bot.reply_to(msg, f"❌ الخطأ: {str(e)}")
-        
 
