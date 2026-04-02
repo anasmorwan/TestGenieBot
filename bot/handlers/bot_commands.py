bd6458b110ccc141913d29898873e93a163ac806
anasmorwan <anasmorwan5@gmail.com>
2026-04-02 08:52:42 +0200

    Create bot_commands.py

---
+++ bot/handlers/bot_commands.py
@@ -0,0 +1,39 @@
+
+def register(bot):
+  
+    @bot.message_handler(commands=["menu"])
+    def user_info(msg):
+        try:
+            user_id = msg.from_user.id
+            chat_id = msg.chat.id
+            
+            bot_username = bot.get_me().username
+    
+            keyboard = more_options_keyboard(bot_username)
+            
+            
+            base_text = get_message("BASE_TEXT")
+            menu_text = get_message("MORE")
+    
+            # النص المتغير (التحية أو مقدمة مخصصة)
+            welcome_new_user = "<b>👋 مرحباً بك في TestGenie</b>\n\n"
+            welcome_returning_user = "<b>👋 مرحباً بك مجددًا في TestGenie</b>\n\nما الذي ترغب في القيام به اليوم؟\n\n"
+        
+            #if is_user_exist(chat_id):
+            #text = ux_text          
+        
+        
+            #else:    
+            #   text = ux_text
+
+            text = menu_text
+                
+    
+            bot.send_message(chat_id,
+            text=text,
+            reply_markup=keyboard,
+            parse_mode="HTML")
+
+        except Exception as e:
+            bot.reply_to(msg, f"❌ الخطأ: {str(e)}")
+        
