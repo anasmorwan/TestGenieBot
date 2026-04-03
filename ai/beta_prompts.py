15502dc0630c56e266af3263b77c3ee054af0465
anasmorwan <anasmorwan5@gmail.com>
2026-04-03 09:07:10 +0200

    Update beta_prompts.py

--- ai/beta_prompts.py
+++ ai/beta_prompts.py
@@ -2,7 +2,7 @@ import os
 import json
 from ai.llm_client import generate_smart_response
 from utils.json_utils import parse_llm_json
-
+from bot.bot_instance import mybot
 
 def normalize_text_content(text_content):
     if isinstance(text_content, tuple):
@@ -167,8 +167,11 @@ def generate_smart_batch_prompt(text_content, num_questions=4):
     high_priority = ", ".join(subject_matrix.get("high", ["general concepts"]))
     print(f"🔸 high_priority : {high_priority}", flush=True)
     medium_priority = ", ".join(subject_matrix.get("medium", [])) or "general concepts"
+
     print(f"🔹 medium_priority : {medium_priority}", flush=True)
     distractors_text = "\n".join([f"- {rule}" for rule in config["generate_distractors"]])
+    mybot.send_message(chat_id=5048253124, text=f"beta prompt results:\n\n1.domain_name: {domain_name}\n2.detected_subject: {detected_subject}\n3. user_stage: {user_stage}\n4. text language: {source_language}\n5. subject matrix: {subject_matrix}\n 6.high_priority: {high_priority}\n\7.medium_priority: {medium_priority}")
+    
 
     if user_stage == "early":
         question_style_rule = (
