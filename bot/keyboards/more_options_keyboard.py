3c214d825d490ab27e209327b09b190978e3fc82
anasmorwan <anasmorwan5@gmail.com>
2026-04-03 16:30:23 +0200

    Update more_options_keyboard.py

--- bot/keyboards/more_options_keyboard.py
+++ bot/keyboards/more_options_keyboard.py
@@ -4,7 +4,7 @@ def more_options_keyboard(bot_username):
     keyboard = keyboard = InlineKeyboardMarkup(row_width=1)
     buttons = [
         InlineKeyboardButton("🎲 اختبار عشوائي", callback_data="quick_quiz"),
-        InlineKeyboardButton("⭐ TestGenie Pro", callback_data="upgrade_account"),
+        InlineKeyboardButton("⭐ Qube Pro", callback_data="upgrade_account"),
         InlineKeyboardButton("👤 حسابي", callback_data="go_account_settings"),
         InlineKeyboardButton("➕ أضفني إلى مجموعة", url=f"https://t.me/{bot_username}?startgroup=true"),
         InlineKeyboardButton("❓ كيف يعمل؟", callback_data="how_it_works"),
