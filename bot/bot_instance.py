9731866bbf418ccc9b3dcc5de665d473ea174277
anasmorwan <anasmorwan5@gmail.com>
2026-04-03 18:51:14 +0200

    Update bot_instance.py

--- bot/bot_instance.py
+++ bot/bot_instance.py
@@ -9,11 +9,13 @@ load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 
-mybot = telebot.TeleBot(BOT_TOKEN, threaded=True, num_threads=20)
+mybot = telebot.TeleBot(BOT_TOKEN, threaded=False)
 
 
 def set_webhook():
     mybot.remove_webhook()
-    mybot.set_webhook(url=f"{WEBHOOK_URL}/{BOT_TOKEN}")
+    # إضافة drop_pending_updates=True تمسح الرسائل المتراكمة القديمة
+    mybot.set_webhook(url=f"{WEBHOOK_URL}/{BOT_TOKEN}", drop_pending_updates=True)
+
     logging.info(f"🌍 Webhook set at {WEBHOOK_URL}/{BOT_TOKEN}")
     
