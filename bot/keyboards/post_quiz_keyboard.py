50bdb9d8bf168a4938201ef1f4f70f97b28e7bbe
anasmorwan <anasmorwan5@gmail.com>
2026-04-03 16:29:38 +0200

    Update post_quiz_keyboard.py

--- bot/keyboards/post_quiz_keyboard.py
+++ bot/keyboards/post_quiz_keyboard.py
@@ -8,10 +8,9 @@ def post_to(user_id):
 
     btn_postgroup = InlineKeyboardButton("👥 إرسال لمجموعة (مجاني)", callback_data="upgrade_account")
     btn_postchannel = InlineKeyboardButton("📢 نشر في قناة (للمديرين)", callback_data="main_menu")
-    btn_back = InlineKeyboardButton("⬅️ رجوع", callback_data="main_menu")
     
 
-    markup.add(btn_refer, btn_upgrade, btn_back)
+    markup.add(btn_refer, btn_upgrade)
     return markup
 
 
@@ -22,7 +21,6 @@ def post_to_channel(user_id):
 
     btn_deep_link = InlineKeyboardButton("🔗 رابط اختبار تفاعلي (مجاني)", callback_data="upgrade_account")
     btn_polls = InlineKeyboardButton("📊 نشر كاستطلاعات متتالية 🔒 (Pro)", url=f"https://t.me/share/url?url=https://t.me/testprog123bot?start=ref_{ref_code}&text=🔥 جرب هذا البوت! يحول أي ملف لاختبار تفاعلي خلال ثواني") 
-    btn_back = InlineKeyboardButton("⬅️ رجوع", callback_data="main_menu")
-
-    markup.add(btn_refer, btn_upgrade, btn_back)
+    
+    markup.add(btn_refer, btn_upgrade)
     return markup
