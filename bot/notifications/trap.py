14bb5a499feb0013a1f886f5f2cc76138a02d1d7
anasmorwan <anasmorwan5@gmail.com>
2026-04-04 09:43:17 +0200

    Update trap.py

--- bot/notifications/trap.py
+++ bot/notifications/trap.py
@@ -1,8 +1,22 @@
 from bot.bot_instance import mybot
 from services.user_trap import should_show_daily
-
-
+from storage.sqlite_db import get_connection
 
 
 if should_show_daily(user_id):
-  mybot.send_message()
+    user_ids = get_all_user_ids(cursor)
+    for user_id in user_ids
+    mybot.send_message(chat_id =user_id, )
+
+
+def get_all_user_ids():
+    conn = get_connection
+    cursor = conn.cursor()
+  
+    try:
+        cursor.execute("SELECT user_id FROM users")
+        results = cursor.fetchall()
+        return [row[0] for row in results]
+    except Exception as e:
+        print(f"خطأ في استرجاع user_ids: {e}")
+        return []
