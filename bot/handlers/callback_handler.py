4ee71724faa7f26030709c46711d809624f9a900
anasmorwan <anasmorwan5@gmail.com>
2026-04-05 12:09:16 +0200

    Update callback_handler.py

--- bot/handlers/callback_handler.py
+++ bot/handlers/callback_handler.py
@@ -174,8 +174,9 @@ def register(bot):
                     if review_count > 0:
                         mistakes = get_recent_mistakes(user_id, review_count)
                     
-                        QuizManager.start_mistakes_review(chat_id, mistakes, bot)
-                        start_mistakes_review(self, chat_id, object, bot):
+                        quiz_manager = QuizManager()  # إنشاء كائن
+                        quiz_manager.start_mistakes_review(chat_id, mistakes, bot)
+                        
                 except Exception as e:
                     bot.send_message(chat_id, f"CALLBACK ERROR: {str(e)}")
                 finally:
