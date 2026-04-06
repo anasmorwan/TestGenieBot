8621dafa0ce13ac85590977523423783914d0e78
anasmorwan <anasmorwan5@gmail.com>
2026-04-06 13:07:54 +0200

    Update payment_handler.py

--- bot/handlers/payment_handler.py
+++ bot/handlers/payment_handler.py
@@ -1,13 +1,14 @@
 # bot/handlers/payment_handler.py
 from services.usage import activate_subscription
 from services.backup_service import safe_backup, backup_all
-
+from services.user_trap import update_last_active
 
 def register(bot):
 
     @bot.message_handler(content_types=['successful_payment'])
     def got_payment(message):
         user_id = message.from_user.id
+        update_last_active(user_id)
         payload = message.successful_payment.invoice_payload
 
         if payload == "pro_plan":
