4a61b71c744e95667193f740860ca6d95a89e0cc
anasmorwan <anasmorwan5@gmail.com>
2026-04-11 21:16:17 +0200

    Update menu.py

--- bot/handlers/menu.py
+++ bot/handlers/menu.py
@@ -10,17 +10,17 @@ from bot.keyboards.main_menu import main_menu_keyboard, smart_ui_keyboard
 
 
 def send_main_menu(chat_id, message_id=None):
-    
 
     bot_username = mybot.get_me().username
+      
+
     
-    base_text = get_message("BASE_TEXT")
-    ux_text = get_message("UX_TEXT")
-    limit = calculate_daily_review_limit(chat_id)
-    
+    limit = calculate_daily_review_limit(chat_id) 
     mistakes_stat = get_user_mistakes_stats(chat_id)
-    total_mistakes = mistakes_stat.get("total_mistakes")
-    recent_mistakes = mistakes_stat.get("recent_mistakes")
+
+    
+    total_mistakes = mistakes_stat.get("total_mistakes") 
+    
     total_questions = get_normal_questions_total(chat_id)
     todays_attempts = get_today_attempts(chat_id)
     todays_score = 0
@@ -30,25 +30,27 @@ def send_main_menu(chat_id, message_id=None):
         print("لا توجد محاولات لهذا اليوم")
 
     
-    new_text = get_message("MAIN_MENU", total_today=todays_score, mistakes_count=total_mistakes)
+    smart_ui_text = get_message("MAIN_MENU", total_today=todays_score, mistakes_count=total_mistakes)
+    base_text = get_message("BASE_TEXT")
+    ux_text = get_message("UX_TEXT")
     
-    # النص المتغير (التحية أو مقدمة مخصصة)
-    welcome_new_user = "<b>👋 مرحباً بك في Qube</b>\n\n"
-    welcome_returning_user = "<b>أهلاً بك، أنا Qube.. كيف يمكنني أن أختبر ذكاءك اليوم؟\n\n"
+
+    if total_mistakes == 0:
+        smart_ui_text = get_message("MENU_NO_MISTAKES")
     
+
     text = ux_text
     keyboard = None
     parse_mode = "HTML"
 
     
     if is_user_exist(chat_id):
-        text = new_text
+        text = smart_ui_text
         keyboard = smart_ui_keyboard(limit)
-        parse_mode = "HTML"
         
     
     if message_id:
-        text = new_text
+        text = smart_ui_text
         keyboard = smart_ui_keyboard(limit)
         
         mybot.edit_message_text(
