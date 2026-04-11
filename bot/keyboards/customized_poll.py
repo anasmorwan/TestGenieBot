aaebe5700eeb08cfd7881a1bc5805f1f3adad8fd
anasmorwan <anasmorwan5@gmail.com>
2026-04-11 16:37:19 +0200

    Update customized_poll.py

--- bot/keyboards/customized_poll.py
+++ bot/keyboards/customized_poll.py
@@ -2,7 +2,7 @@ from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
 
-def get_poll_customize_keyboard(selected_tone="ودي", selected_goal="رأي"):
+def get_poll_customize_keyboard(selected_tone="ودي", selected_goal="رأي", set=False):
     markup = InlineKeyboardMarkup(row_width=3)
 
     # 🎯 الهدف
@@ -14,7 +14,7 @@ def get_poll_customize_keyboard(selected_tone="ودي", selected_goal="رأي"):
     markup.row(*goal_buttons)
 
     # 🎭 الطابع
-    tones = ["😊 ودي", "🎯 رسمي", "🔥 حماسي"]
+    tones = ["😊 ودي", "🔥 حماسي", "🎯 رسمي"]
     tone_buttons = []
     for t in tones:
         text = f"✅ {t}" if t == selected_tone else t
@@ -22,13 +22,15 @@ def get_poll_customize_keyboard(selected_tone="ودي", selected_goal="رأي"):
     markup.row(*tone_buttons)
 
     # ⚙️ خيارات إضافية
+    text = "✅ تم حفظ الإعدادات" if set else "⚙️ حفظ"   
     markup.row(
-        InlineKeyboardButton("⚙️ حفظ", callback_data="poll_advanced")
+        InlineKeyboardButton(text, callback_data="poll_advanced")
     )
 
     # 🚀 توليد
+    text_1 = "🚀 إعادة توليد الآن" if set else "🚀 إعادة توليد الاستطلاع"
     markup.row(
-        InlineKeyboardButton("🚀 إعادة توليد الاستطلاع", callback_data="regenerate")
+        InlineKeyboardButton(text_1, callback_data="regenerate")
     )
 
     return markup
