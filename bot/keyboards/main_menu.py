c69b2a73588efdd0b58234d3902e7d82e51f4eeb
anasmorwan <anasmorwan5@gmail.com>
2026-04-11 19:59:22 +0200

    Update main_menu.py

--- bot/keyboards/main_menu.py
+++ bot/keyboards/main_menu.py
@@ -13,5 +13,17 @@ def main_menu_keyboard(bot_username):
     return keyboard
 
 
+def smart_ui_keyboard(mistakes):
+    markup = InlineKeyboardMarkup(row_width=1)
+
+    
+    btn_mistakes = InlineKeyboardButton(f"📖 مراجعة أخطائي: {mistakes}", callback_data=f"start_challenge:mistakes:{mistakes}")
+    btn_random_quiz = InlineKeyboardButton("🧠 اختبرني عشوائياً", callback_data=f"start_challenge:user_review")
+    btn_settings = InlineKeyboardButton("⚙️ الإعدادات", callback_data=f"more_options")
+    
+
+
+    markup.add(btn_mistakes, btn_random_quiz, btn_settings)
+    return markup
  
     
