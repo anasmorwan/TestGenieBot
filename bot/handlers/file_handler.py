9339c39b8fd796dbfaee0b214808315c0870afcd
anasmorwan <anasmorwan5@gmail.com>
2026-04-12 07:51:28 +0200

    Update file_handler.py

--- bot/handlers/file_handler.py
+++ bot/handlers/file_handler.py
@@ -65,8 +65,9 @@ def register(bot):
                     show_referral_message(bot, chat_id, user_id)
                     return
                 
-                show_channel_invitation(bot, chat_id)  
-                return  # ❗ هذا هو المفتاح
+                else:
+                    show_channel_invitation(bot, chat_id)  
+                    return  # ❗ هذا هو المفتاح
                 
                 
             # 👇 استهلك محاولة
