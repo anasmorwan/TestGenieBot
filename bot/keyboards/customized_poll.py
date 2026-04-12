99c4f9eb164a46a9703ef451119021c41cab38c3
anasmorwan <anasmorwan5@gmail.com>
2026-04-12 07:05:48 +0200

    Update customized_poll.py

--- bot/keyboards/customized_poll.py
+++ bot/keyboards/customized_poll.py
@@ -1,18 +1,5 @@
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-mapping = {
-    "goal": {
-        "قرار": "🤝قرار",
-        "مقارنة": "⚖️ مقارنة",
-        "رأي": "📊 رأي"
-    },
-    "tone": {
-        "ودي": "😊 ودي",
-        "رسمي": "🎯 رسمي",
-        "حماسي": "🔥 حماسي"
-    },
-}
-
 
 def get_poll_customize_keyboard(selected_tone="ودي", selected_goal="رأي", set=False):
     markup = InlineKeyboardMarkup(row_width=3)
@@ -20,18 +7,17 @@ def get_poll_customize_keyboard(selected_tone="ودي", selected_goal="رأي", 
     # 🎯 الهدف
     goals = ["📊 رأي", "⚖️ مقارنة", "🤝 قرار"]
     goal_buttons = []
-    normalized_goal = mapping.get("goal").get(selected_goal)
+    
     for g in goals:
-        text = f"✅ {g}" if g == normalized_goal else g
+        text = f"✅ {g}" if g == goals else g
         goal_buttons.append(InlineKeyboardButton(text, callback_data=f"goal_{g}"))
     markup.row(*goal_buttons)
 
     # 🎭 الطابع
     tones = ["😊 ودي", "🔥 حماسي", "🎯 رسمي"]
-    normalized_tone = mapping.get("tone").get(selected_tone)
     tone_buttons = []
     for t in tones:
-        text = f"✅ {t}" if t == normalized_tone else t
+        text = f"✅ {t}" if t == tones else t
         tone_buttons.append(InlineKeyboardButton(text, callback_data=f"tone_{t}"))
     markup.row(*tone_buttons)
 
