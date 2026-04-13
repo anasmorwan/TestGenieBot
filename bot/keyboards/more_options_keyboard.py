33ab05728cbc4b549b2127e78a1ed341b01d30e5
anasmorwan <anasmorwan5@gmail.com>
2026-04-14 00:10:30 +0200

    Update more_options_keyboard.py

--- bot/keyboards/more_options_keyboard.py
+++ bot/keyboards/more_options_keyboard.py
@@ -3,7 +3,7 @@ from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 def more_options_keyboard(bot_username):
     keyboard = keyboard = InlineKeyboardMarkup(row_width=1)
     buttons = [
-        InlineKeyboardButton("🎲 اختبار عشوائي", callback_data="quick_quiz"),
+        InlineKeyboardButton("📆 جدولة إختبار", callback_data="scheduled_quiz"),
         InlineKeyboardButton("⭐ Qube Pro", callback_data="upgrade_account"),
         InlineKeyboardButton("👤 حسابي", callback_data="go_account_settings"),
         InlineKeyboardButton("➕ أضفني إلى مجموعة", url=f"https://t.me/{bot_username}?startgroup=true"),
