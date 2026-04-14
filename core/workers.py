5a64eed74bef1397d7296e4edf7bdfcafce6dff7
anasmorwan <anasmorwan5@gmail.com>
2026-04-14 10:29:12 +0200

    Update workers.py

--- core/workers.py
+++ core/workers.py
@@ -1,1 +1,13 @@
+import threading
+from core.task_queue import task_queue
 
+from services import handlers  # أو أي functions عندك
+
+def worker():
+    while True:
+        priority, task = task_queue.get()
+
+        try:
+            process_task(task)
+        finally:
+            task_queue.task_done()
