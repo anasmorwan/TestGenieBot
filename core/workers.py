8355252acf9f3a1ec4610152fc307eb5fbfec84e
anasmorwan <anasmorwan5@gmail.com>
2026-04-14 17:45:32 +0200

    Update workers.py

--- core/workers.py
+++ core/workers.py
@@ -56,7 +56,49 @@ def process_task(task):
         
 
     elif task_type == "text_generate_quiz": 
-        generate_quizzes_from_text(task)
+        if not quizzes or len(quizzes) == 0:
+            print(f"DEBUG: [User: {user_id}] Quiz generation returned EMPTY result.", flush=True)
+            bot.send_message(chat_id, "❌ فشل توليد الاختبار. تأكد أن النص يحتوي على معلومات كافية.")
+            return
+
+        if state == "scheduled_quiz":
+            quiz_code = store_quiz(user_id, quizzes, schedule=True)
+            bot.send_message(
+                chat_id=chat_id,
+                text=get_message("SCHEDULED_QUIZ_READY"),
+                parse_mode="HTML"
+            )
+            return
+        quiz_code = store_quiz(user_id, quizzes)
+                    
+        maybe_cleanup()
+        quiz_len = len(quizzes)
+
+        action = random.choice(["delete", "edit"])
+        reply_markup = quiz_keyboard(quiz_code)
+        if action == "delete":
+            bot.delete_message(chat_id, message_id=waiting_msg.message_id)
+            bot.send_message(
+                chat_id=chat_id,
+                text=get_message("QUIZ_CREATED", count=quiz_len),
+                parse_mode="HTML"
+            )
+            time.sleep(2)
+        else:
+            bot.edit_message_text(
+                chat_id=chat_id,
+                message_id=msg_id,
+                text=get_message("QUIZ_CREATED", count=quiz_len),
+                parse_mode="HTML"
+                )
+            time.sleep(2)
+            
+        if not quiz_manager.start_quiz(chat_id, quiz_code, bot, is_shared_user=False):
+            bot.edit_message_text(
+            chat_id=chat_id,
+            message_id=waiting_msg.message_id,
+            text="😵 لم يتم العثور على هذا الاختبار أو انتهت صلاحيته."
+        )
 
     elif task_type == "file_generate_quiz":
         maybe_cleanup() 
