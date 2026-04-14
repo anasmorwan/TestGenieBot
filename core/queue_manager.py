ec4e30b687055e0ae77ea1e8fe960cca3990d2e2
anasmorwan <anasmorwan5@gmail.com>
2026-04-14 10:29:00 +0200

    Update queue_manager.py

--- core/queue_manager.py
+++ core/queue_manager.py
@@ -1,3 +1,11 @@
 from queue import PriorityQueue
 
 task_queue = PriorityQueue()
+
+
+def add_task(priority, task: dict):
+    task_queue.put((priority, task))
+
+
+def get_task():
+    return task_queue.get()
