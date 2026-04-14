f978ccfcc466e22d72c542dd84909408fa28e087
anasmorwan <anasmorwan5@gmail.com>
2026-04-14 12:13:45 +0200

    Update queue_manager.py

--- core/queue_manager.py
+++ core/queue_manager.py
@@ -3,8 +3,14 @@ from queue import PriorityQueue
 task_queue = PriorityQueue()
 delayed_queue = PriorityQueue()
 
-def add_task(priority, task: dict):
-    task_queue.put((priority, task))
+
+
+def add_task(task: dict):
+    if task.get("run_at"):
+        delayed_queue.put((task["run_at"], task))
+    else:
+        task_queue.put((task.get("priority", 1), task))
+
 
 
 def get_task():
